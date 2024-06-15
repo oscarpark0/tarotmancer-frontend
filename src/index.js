3c85b064e29b7a14c86ec64a0e02c7337d5dd13b
Oscar Park <115663638+oscarpark0@users.noreply.github.com>
2024-06-14 23:14:46 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
-import { SpeedInsights } from "@vercel/speed-insights/react"; // Import SpeedInsights
+import { SpeedInsights } from "@vercel/speed-insights/react"; 
 
 const root = createRoot(document.getElementById('root'));
 root.render(
@@ -18,7 +18,7 @@ root.render(
     useRefreshTokens={true}
     cacheLocation="localstorage"
   >
-    <SpeedInsights /> {/* Wrap your app with SpeedInsights */}
+    <SpeedInsights /> 
     <App />
   </Auth0Provider>
 );

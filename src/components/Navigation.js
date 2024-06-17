0dfe8e2b6b784267fad13eea834c47c783a50e66
Oscar Park <115663638+oscarpark0@users.noreply.github.com>
2024-06-17 17:43:03 -0400

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import SignUp from './SignUp';
-import Login from './Login';
 
 function Navigation() {
-  const { isAuthenticated } = useAuth0();
+  const { isAuthenticated, loginWithRedirect } = useAuth0();
 
   return (
     <nav>
       {/* Other navigation items */}
       {!isAuthenticated && (
-        <>
-          <SignUp />
-            
-          <Login />
-        </>
+        <button onClick={() => loginWithRedirect()}>Log In</button>
       )}
     </nav>
   );

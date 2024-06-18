544dbe88885ade880afa6ec8990d2a88909dd19a
Oscar Park <115663638+oscarpark0@users.noreply.github.com>
2024-06-17 22:35:11 -0400

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL = "https://backend-tarotmancer-53d9d741cb25.herokuapp.com";
-export const TAROT_IMAGE_BASE_URL = 'https://tarot-mancer.twic.pics/tarot/';
+export const TAROT_IMAGE_BASE_URL = 'https://tarot-mancer.twic.pics/tarot';
 export const COHERE_API_KEY='9qNcM0N4ry0piftMirHZ5YrBcNv2UFhLw8nlYueb'

5559ba30c4a5cd81df48b61c97e7ed21782fa50c
Oscar Park <115663638+oscarpark0@users.noreply.github.com>
2024-06-17 22:53:09 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const tailwindcss = require('tailwindcss');
 
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media',
   theme: {
 
         test: /\.css$/,

d2857f9518d031b344a8fb36c3cb14650d3c3f60
Oscar Park <115663638+oscarpark0@users.noreply.github.com>
2024-06-25 12:01:03 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ root.render(
     domain={process.env.REACT_APP_AUTH0_DOMAIN}
     clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
     redirectUri={window.location.origin}
-    audience={process.env.REACT_APP_AUTH0_AUDIENCE}
   >
     <SpeedInsights /> 
     <App />

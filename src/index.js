b9816fd3010be6aabedc221e095117cf99b08963
Oscar Park <115663638+oscarpark0@users.noreply.github.com>
2024-06-25 11:52:24 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,14 +5,20 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { SpeedInsights } from "@vercel/speed-insights/react"; 
 import { Analytics } from '@vercel/analytics/react';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <>
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH0_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+    redirectUri={window.location.origin}
+    audience={process.env.REACT_APP_AUTH0_AUDIENCE}
+  >
     <SpeedInsights /> 
     <App />
     <Analytics />
-  </>
+  </Auth0Provider>
 );
 
 

a241fe2956771ec58d0eca8d6dc8dd83f940bfef
Oscar Park <115663638+oscarpark0@users.noreply.github.com>
2024-06-26 12:24:01 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { KindeProvider } from "@kinde-oss/kinde-auth-react";
+import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { SpeedInsights } from "@vercel/speed-insights/react"; 
+import { Analytics } from '@vercel/analytics/react';
+import { KindeProvider } from "@kinde-oss/kinde-auth-react";
 
 const root = createRoot(document.getElementById('root'));
 root.render(
@@ -12,7 +16,12 @@ root.render(
       redirectUri={process.env.REACT_APP_KINDE_REDIRECT_URI}
       logoutUri={process.env.REACT_APP_KINDE_LOGOUT_URI}
     >
+      <SpeedInsights /> 
       <App />
+      <Analytics />
     </KindeProvider>
   </React.StrictMode>
 );
+
+
+reportWebVitals();

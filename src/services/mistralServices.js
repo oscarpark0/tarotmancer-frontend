584420c02e964847a94ae9148e224863f17a9a19
Oscar Park <115663638+oscarpark0@users.noreply.github.com>
2024-07-18 17:04:40 -0400

    Update mistralServices.js

--- src/services/mistralServices.js
+++ src/services/mistralServices.js
@@ -6,7 +6,7 @@ export const getMistralResponse = async (message, onNewResponse, onResponseCompl
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        model: "mistral-nemo",
+        model: "mistral-nemo-latest",
         messages: [{ role: "user", content: message }],
         stream: true,
         max_tokens: 1000

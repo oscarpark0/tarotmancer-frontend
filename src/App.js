0ffa6a9ade01d07c0a3ca21e785a8e046f66b05f
Oscar Park <115663638+oscarpark0@users.noreply.github.com>
2024-07-30 09:37:06 -0400

    matomo

--- src/App.js
+++ src/App.js
@@ -300,6 +300,12 @@ function AppContent() {
 function App() {
   useEffect(() => {
     initializeTranslations();
+
+    // Matomo tracking code
+    var _mtm = window._mtm = window._mtm || [];
+    _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src='https://cdn.matomo.cloud/tarotmancer.matomo.cloud/container_YKllof39.js'; s.parentNode.insertBefore(g,s);
   }, []);
 
   return (

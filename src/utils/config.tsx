9f68efe8816451731ed9b70dd9328005e6f00321
Oscar Park <115663638+oscarpark0@users.noreply.github.com>
2024-07-30 20:51:46 -0400

    Update config.tsx

--- src/utils/config.tsx
+++ src/utils/config.tsx
@@ -1,3 +1,3 @@
-export const TAROT_IMAGE_BASE_URL: string = 'https://tarot.twic.pics/tarot/';
+export const TAROT_IMAGE_BASE_URL: string = 'https://tarot.twic.pics/tarot';
 export const COHERE_API_KEY: string = 'KXHZKJrH32nBDEUtFVPNDZZGO3pHUsSzi5h1BnAK';
 export const API_BASE_URL: string = process.env.REACT_APP_BASE_URL || "https://backend-tarotmancer-53d9d741cb25.herokuapp.com";

ec12e7a96d711fed30361964827eb85f2e0aa229
oscapark0 <tmbrou@protonmail.com>
2025-03-01 18:59:20 -0500

    Update config.tsx

--- src/utils/config.tsx
+++ src/utils/config.tsx
@@ -1,5 +1,5 @@
 export const TAROT_IMAGE_BASE_URL: string = 'https://ik.imagekit.io/tarotmancer';
-// Try the main tarotmancer backend URL instead
-export const API_BASE_URL: string = process.env.REACT_APP_BASE_URL || "https://tarotmancer-backend.herokuapp.com";
+// Hardcode the backend URL to ensure it's correctly set
+export const API_BASE_URL: string = "https://tarotmancer-backend.herokuapp.com";
 export const IMAGEKIT_PUBLIC_KEY: string = process.env.REACT_APP_IMAGEKIT_PUBLIC_KEY || '';
 export const IMAGEKIT_URL_ENDPOINT: string = 'https://ik.imagekit.io/tarotmancer';
